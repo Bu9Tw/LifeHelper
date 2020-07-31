cd889fff59c5f2ec087156729dc3989dc4f4e114
Bob <a@a.a.a>
2020-07-31 16:48:54 +0800

    Vue顯示職缺畫面

--- src/LifeHepler/LifeHepler/ClientApp/src/App.vue
+++ src/LifeHepler/LifeHepler/ClientApp/src/App.vue
@@ -1,28 +1,21 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />-->
+    <JobInfos />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import JobInfos from "./components/JobInfos.vue";
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./Content/Site.css";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    JobInfos,
+  },
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
